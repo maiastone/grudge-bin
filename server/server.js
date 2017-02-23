e319000c7f274fac73e52a4ad35f5810d8e60c02
Maia Stone <maiadov@gmail.com>
2017-02-23 11:15:38 -0700

    Adds get route for app.locals

--- server/server.js
+++ server/server.js
@@ -2,14 +2,34 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const http = require('http');
+const bodyParser = require('body-parser');
+const md5 = require('md5');
 const environment = process.env.NODE_ENV || 'development';
 const port =  process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
-app.get('/*', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.locals.grudges = [
+  {
+    id: 1,
+    name: 'Creature',
+    offense: 'Slimy',
+    forgiven: false,
+    date: Date.now()
+  }
+]
+
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
+
+app.get('/api/grudges', (req, res) => {
+  const grudges = app.locals.grudges;
+  res.json(grudges);
+})
 
 const server = http.createServer(app)
 .listen(port, () => {
