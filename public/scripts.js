f303490e1aa04a40751c678396fd584bde4b3e6a
Maia Stone <maiadov@gmail.com>
2017-02-25 14:30:54 -0700

    Add style for unique box and instructions

--- public/scripts.js
+++ public/scripts.js
@@ -117,14 +117,14 @@ $('.grudge-container').on('click', 'li', function(e) {
 function appendDetails(response) {
 	$('.unique-grudge-container').html('');
 	$('.unique-grudge-container').append(`
-		<h3>${response.data[0].name}</h3>
+		<h3 class='name'>${response.data[0].name}</h3>
 		<h3>${response.data[0].offense}</h3>
-		<h3>${response.data[0].date}</h3>
+		<h3 class='date'>${response.data[0].date}</h3>
 		<button id=${response.data[0].id} class='forgive'>Forgive</button>
 	`)
 };
 
-$('.unique-grudge-container').on('click', 'button', function(e) {
+$('.unique-grudge-container').on('click', '.forgive', function(e) {
 	let id = e.target.id
 	axios.patch(`/api/grudges/${id}`)
   .then(function(response) {
