2bdc8b1abdad4b1a80928d117ff32dc2e3c8e610
Maia Stone <maiadov@gmail.com>
2017-02-27 16:02:21 -0700

    Passes unit tests for count functions

--- public/tests/units.spec.js
+++ public/tests/units.spec.js
@@ -5,13 +5,15 @@ describe('Units', function() {
       name: 'maia',
       id: 1,
       offense: 'stressed',
-      date: 'Feb 27th 2017, 15:08'
+      date: 'Feb 27th 2017, 15:08',
+      forgiven: true,
     },
     {
       name: 'jamie',
       id: 2,
       offense: 'late',
-      date: 'Feb 24th 2017, 2:08'
+      date: 'Feb 24th 2017, 2:08',
+      forgiven: false,
     },
   ]
 
@@ -25,4 +27,12 @@ describe('Units', function() {
     expect(subject[0].date).to.equal('Feb 24th 2017, 2:08')
     expect(subject[1].date).to.equal('Feb 27th 2017, 15:08')
   });
+  it('should count Unforgiven grudges', function() {
+    let subject = countUnforgivenGrudges(grudges);
+    expect(subject).to.equal(1);
+  })
+  it('should count Forgiven grudges', function() {
+    let subject = countForgivenGrudges(grudges);
+    expect(subject).to.equal(1);
+  })
 })
